fc82783507b169fb91f48575c23404044a96d501
xynree <xynree@gmail.com>
2021-02-27 16:10:03 -0800

    ll

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import './tailwind.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+
+                {/* DECORATION */}
+
+
+      <div className="decelements w-full overflow-hidden h-full fixed">
+        
+
+  <div className='redline'></div>
+  <div className="ltblueline"></div>
+  <div className='pinkline z-10'></div>
+
+       </div>
+
+
+                {/* DECORATION */}
+
+      <div className='
+      title
+      fixed
+drkgrntxt     
+      hirakaku
+      w-40
+      transform 
+      -rotate-90
+      -right-16
+      top-2/3
+      text-lg
+      z-30
+      '>rating room</div>
+
+      <div className='producttitle relative  top-14 left-2 text-right darkbluetxt text-4xl w-40 sunflora       z-30
+'>"Have'A Corn Chip"</div>
+
+
+{/* SORTING COMPONENT */}
+      <div>Sorting</div>
+
+{/* IMAGE COMPONENT */}
+    <div className='z-30 imagebox image right-0 bg-contain'></div>
+    <div className='about bottom-0 fixed '>(ABOUT)</div>
+    <div>(FULL LIST COMPONENT)</div>
+
+
+
+
+      <div className='ratingarea  content-evenly flex absolute w-screen h-120  top-64'>
+
+        <div className='flex relative flex-col  flex-1 left-2 mr-2'>
+                {/* (FETCH RATING COMPONENT) */}
+          <div className='xrating sticky top-6 hirakaku z-30 relative'>3/5</div>
+                 {/* (FETCH REVIEW COMPONENT) */}
+          <div className='text-sm  xbodytxt relative timesnewroman overflow-scroll lightbluetxt mt-3 pl-10 pr-4'>blah blah rating from xyn long thing about how great corn chip is. anyway this will be replaced by a component that pushes this info into this area and the ratings may be long so i'm going to bla bla bla and go on to see it go off the page and see where that goes. now bla bla, i say. Blah de Baly to You. </div>
+        </div>
+
+        <div className='flex relative flex-col flex-1 right-2 ml-2'>
+                {/* (FETCH RATING COMPONENT) */}
+          <div className='zrating sticky top-6 hirakaku z-30 relative left-1  '>5/5</div>
+                    {/* (FETCH REVIEW COMPONENT) */}
+          <div className='text-sm zbodytxt relative timesnewroman overflow-scroll lightbluetxt mt-1 pl-4 pr-10'>blah blah rating from zanelong thing about how great corn chip is. anyway this will be replaced by a component that pushes this info into this area and the ratings may be long so i'm going to bla bla bla and go on to see it go off the page and see where that goes. now bla bla, i say. Blah de Baly to You.. Long Rating goes out to all my fans as you can see there are many gathered here today to initialize my lorem ipsum bla ded blah here on the screen my random text to fill letters for you.</div>
+        </div>
+      </div>
+
+      <div className='hidebox bglight fixed top-0 w-3/4 h-36 left-0 right-0 m-auto'></div>
+
+
+
+{/* IMAGE COMPONENTS IN A FLEXBOX */}
+    <div><div><img src='./'/></div><div><img src='./'/></div><div><img src='./'/></div><div><img src='./'/></div></div>
+
+
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
   );
 }
 
